c50a8358ab8838e31094fd27d9fe4d63b8e42995
Vijay Khemlani <vkhemlan@gmail.com>
2010-05-28 23:26:16 -0700

    Same bugfix as before for detection script

--- django/list_repeated_shn.py
+++ django/list_repeated_shn.py
@@ -7,7 +7,7 @@ from common_fetch_methods import *
 
 ntbks = Notebook.objects.all()
 for ntbk in ntbks:
-    shns = StoreHasNotebook.objects.filter(notebook = ntbk).filter(is_available = True)
+    shns = StoreHasNotebook.objects.filter(notebook = ntbk).filter(is_available = True).filter(is_hidden = False)
     stores = set()
     for shn in shns:
         if shn.store in stores:
