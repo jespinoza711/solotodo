40d3d39f794e4531bf3bf8fd253b919cafe1241c
Vijay Khemlani <vkhemlan@gmail.com>
2010-05-28 20:59:54 -0700

    Modification to search script to match substrings also

--- django/solonotebooks/cotizador/utils.py
+++ django/solonotebooks/cotizador/utils.py
@@ -3,7 +3,8 @@ def stringCompare(comp1, comp2):
     comp2_words = comp2.lower().split()
     counter = 0;
     for word in comp2_words:
-        if word in comp1_words:
-            counter += 1;
+        for comp1_word in comp1_words:
+            if word in comp1_word:
+                counter += 1;
             
     return 100.0 * counter / len(comp2_words)
