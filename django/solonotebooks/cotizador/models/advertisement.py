db632b6d90a0a8af01db2d64cc965d46875c8a5e
Vijay Khemlani <vkhemlan@ubuntu-desktop.(none)>
2010-08-28 13:08:35 -0400

    Added common advertisement adding method for all views and impression counting

--- django/solonotebooks/cotizador/models/advertisement.py
+++ django/solonotebooks/cotizador/models/advertisement.py
@@ -5,6 +5,7 @@ class Advertisement(models.Model):
     embedding_html = models.TextField()
     position = models.ForeignKey(AdvertisementPosition)
     target_url = models.TextField()
+    impressions = models.IntegerField()
     
     class Meta:
         app_label = 'cotizador'
