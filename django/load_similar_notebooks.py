a39b6e93a70e92875f164037fb2f0e32e6e1a90d
Vijay Khemlani <vj@solonotebooks.net>
2011-02-11 09:33:51 -0300

    Wordpress update

--- django/load_similar_notebooks.py
+++ django/load_similar_notebooks.py
@@ -3,10 +3,16 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'solonotebooks.settings'
 
 from solonotebooks.cotizador.models import *
 
+notebooks = dict([[ntbk.id, ntbk] for ntbk in Notebook.objects.all()])
+
 for notebook in Notebook.objects.all():
     print notebook
-    similar_notebooks = [str(ntbk.id) for ntbk in notebook.findSimilarNotebooks()]
-    notebook.similar_notebooks = ','.join(similar_notebooks)
+    try:
+        ntbks = [notebooks[int(nid)] for nid in notebook.similar_notebooks.split(',')]
+    except:
+        print '*'
+        similar_notebooks = [str(ntbk.id) for ntbk in notebook.findSimilarNotebooks()]
+        notebook.similar_notebooks = ','.join(similar_notebooks)
 
-    notebook.save()
+        notebook.save()
  
