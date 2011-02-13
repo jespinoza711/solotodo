8c80b0107f4d2291b301ed794562c4f719516474
Vijay Khemlani <vkhemlan@gmail.com>
2011-02-13 18:14:08 -0300

    renamed v2 files to original

--- apache/media/js/processor_script.js
+++
@@ -1,6 +0,0 @@
-/* Functions for the page that displays processor families*/
-
-$(function() {
-    // Have interleaving colors for the rows of processors for a better look
-    $(".processor_table tr:nth-child(odd)").addClass("odd-row");
-});
