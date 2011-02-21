f10ae74fbf68877f7fd85160c55131519d0714fa
Vijay Khemlani <vkhemlan@gmail.com>
2011-02-21 17:23:11 -0300

    * Added small script to load new store products

---
+++ django/load_new_store_products.py
@@ -0,0 +1,16 @@
+import sys
+import os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'solonotebooks.settings'
+
+from datetime import datetime
+from fetch_scripts import *
+from common_fetch_methods import *
+
+def main():
+    p = eval(sys.argv[1] + '()')
+    get_store_products(p)
+                
+if __name__ == '__main__':
+    print datetime.now()
+    main()
+    
