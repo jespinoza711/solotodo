426d594b8a84cdef10a3633fe8c238216b0df561
Vijay Khemlani <vkhemlan@gmail.com>
2011-02-23 13:34:56 -0300

    * Added ScreenDigitalTuner to admin * Added custom ordering to ScreenDigitalTuner

--- django/solonotebooks/cotizador/models/screen_digital_tuner.py
+++ django/solonotebooks/cotizador/models/screen_digital_tuner.py
@@ -1,11 +1,12 @@
 from django.db import models
 
 class ScreenDigitalTuner(models.Model):
+    ordering = models.IntegerField()
     name = models.CharField(max_length = 255)
     
     def __unicode__(self):
         return self.name
             
     class Meta:
-        ordering = ['name']
+        ordering = ['ordering']
         app_label = 'cotizador'
