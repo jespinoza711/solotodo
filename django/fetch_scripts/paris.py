dda457a96537a17495841540fc1bbbf9760b63b2
Vijay Khemlani <vkhemlan@gmail.com>
2011-02-24 10:32:57 -0300

    * Added screen fetching to Magens, NotebookCenter and Paris scripts

--- django/fetch_scripts/paris.py
+++ django/fetch_scripts/paris.py
@@ -50,7 +50,10 @@ class Paris:
         products_data = []
         
         urls = [
-                    'http://www.paris.cl/webapp/wcs/stores/servlet/categoryTodos_10001_40000000577_-5_51049202_18877035_si_2__18877035,50999203,51049192,51049202_'
+                    # Notebooks
+                    'http://www.paris.cl/webapp/wcs/stores/servlet/categoryTodos_10001_40000000577_-5_51049202_18877035_si_2__18877035,50999203,51049192,51049202_',
+                    # LCD
+                    'http://www.paris.cl/webapp/wcs/stores/servlet/categoryTodos_10001_40000000577_-5_51056205_20096521_si_2__20096521,51056194,51056195,51056205_'
                     ]
         
         product_links = []          
