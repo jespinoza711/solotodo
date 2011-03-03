78b3b217ecf7dabdc71fd5f79150fa0d80e7658d
Vijay Khemlani <vkhemlan@gmail.com>
2011-03-03 11:04:22 -0300

    * Removed old unused scripts * Added shp to product to replace old "is_available" and "min_price"

---
+++ django/legacy_turn_is_available_into_shp.py
@@ -0,0 +1,27 @@
+import sys
+import os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'solonotebooks.settings'
+
+from datetime import datetime
+from solonotebooks.fetch_scripts import *
+from solonotebooks.cotizador.models import *
+from common_fetch_methods import *
+
+def main():
+    products = Product.objects.all()
+    for product in products:
+        shps = product.storehasproduct_set.filter(shpe__isnull = False).order_by('shpe__latest_price')
+        if shps:
+            shp = shps[0]
+            if not product.is_available:
+                print str(shp.id) + ' ' + str(shp)
+            #product.shp = shp
+            #product.save()
+        else:
+            if product.is_available:
+                print str(product.id) + ' ' + str(product)
+                
+if __name__ == '__main__':
+    print datetime.now()
+    main()
+    
