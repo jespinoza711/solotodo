9746a4079e29f3c0edddee18445242b76606840f
Vijay Khemlani <vkhemlan@gmail.com>
2011-03-06 16:25:45 -0300

    * Removed name field from old brand models

--- django/solonotebooks/cotizador/models/notebook_brand.py
+++ django/solonotebooks/cotizador/models/notebook_brand.py
@@ -2,16 +2,14 @@ from django.db import models
 from . import InterfaceBrand
 
 class NotebookBrand(models.Model):
-    name = models.CharField(max_length = 255)
     brand = models.ForeignKey(InterfaceBrand)
     
     def __unicode__(self):
         return unicode(self.brand)
         
     def raw_text(self):
-        return self.name        
+        return unicode(self)      
     
     class Meta:
         app_label = 'cotizador'
-        verbose_name = 'Notebook brand'
-        ordering = ['name']
+        ordering = ['brand']
--- django/solonotebooks/cotizador/models/screen_brand.py
+++ django/solonotebooks/cotizador/models/screen_brand.py
@@ -2,15 +2,14 @@ from django.db import models
 from . import InterfaceBrand
 
 class ScreenBrand(models.Model):
-    name = models.CharField(max_length = 255)
     brand = models.ForeignKey(InterfaceBrand)
     
     def __unicode__(self):
         return unicode(self.brand)
         
     def raw_text(self):
-        return self.name
-            
+        return unicode(self)      
+    
     class Meta:
-        ordering = ['name']
         app_label = 'cotizador'
+        ordering = ['brand']
