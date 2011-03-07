bf0d444fd6a1a3fd632efe21bacaad39d29163be
Vijay Khemlani <vkhemlan@gmail.com>
2011-03-06 23:16:32 -0300

    * Finished video port migration

--- django/solonotebooks/cotizador/models/screen_video_port.py
+++ django/solonotebooks/cotizador/models/screen_video_port.py
@@ -2,7 +2,6 @@ from django.db import models
 from . import InterfaceVideoPort
 
 class ScreenVideoPort(models.Model):
-    name = models.CharField(max_length = 255)
     port = models.ForeignKey(InterfaceVideoPort)
     
     def __unicode__(self):
@@ -13,4 +12,4 @@ class ScreenVideoPort(models.Model):
     
     class Meta:
         app_label = 'cotizador'
-        ordering = ['name']
+        ordering = ['port']
--- django/solonotebooks/cotizador/models/video_card_port.py
+++ django/solonotebooks/cotizador/models/video_card_port.py
@@ -2,7 +2,6 @@ from django.db import models
 from . import InterfaceVideoPort
 
 class VideoCardPort(models.Model):
-    name = models.CharField(max_length = 255)
     port = models.ForeignKey(InterfaceVideoPort)
     
     def __unicode__(self):
@@ -13,4 +12,4 @@ class VideoCardPort(models.Model):
     
     class Meta:
         app_label = 'cotizador'
-        ordering = ['name']
+        ordering = ['port']
