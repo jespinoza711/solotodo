30a5316b6b2e0ee3b5b85af951a378800281e348
Vijay Khemlani <vkhemlan@gmail.com>
2011-03-07 00:07:00 -0300

    * Step 2 of 2 of migrating interface buses

--- django/solonotebooks/cotizador/models/video_card_bus.py
+++ django/solonotebooks/cotizador/models/video_card_bus.py
@@ -2,24 +2,14 @@ from django.db import models
 from solonotebooks.cotizador.models import InterfaceCardBusName, InterfaceCardBusLane, InterfaceCardBus
 
 class VideoCardBus(models.Model):
-    name = models.ForeignKey(InterfaceCardBusName)
-    lane = models.ForeignKey(InterfaceCardBusLane)
-    version = models.DecimalField(max_digits = 2, decimal_places = 1)
     bus = models.ForeignKey(InterfaceCardBus)
     
     def __unicode__(self):
-        str_name = unicode(self.name) 
-        if self.name.show_version_and_lanes:
-            str_name += ' '
-            if str(self.version) != '1.0':
-                str_name += str(self.version) + ' ' 
-            str_name += unicode(self.lane)
-        return str_name
+        return unicode(self.bus)
         
     def raw_text(self):
         return unicode(self)
     
     class Meta:
         app_label = 'cotizador'
-        verbose_name = 'Video card bus'
-        ordering = ['name', 'version', 'lane']
+        ordering = ['bus']
