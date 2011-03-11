75c5d1fbe5a7ab94cc17f17692ce721f15634313
Vijay Khemlani <vkhemlan@gmail.com>
2011-03-11 01:01:46 -0300

    * Added sponsor cap field to stores * Finished the main view for advertisement management * Implemented the add/delete commands for store slots * Started implementing the details for shps

---
+++ apache/media/js/advertisement.js
@@ -0,0 +1,9 @@
+$(function() {
+    $('#reserve_button').click(function() {
+        $('#reserve_products_form').submit()
+    })
+    
+    $('#free_button').click(function() {
+        $('#free_products_form').submit()
+    })
+})
