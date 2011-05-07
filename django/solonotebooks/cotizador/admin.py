139d36c68042b9e81c622b38b5c7f100de7cea98
Vijay Khemlani <vkhemlan@gmail.com>
2011-05-07 11:08:55 -0300

    * Product part number now does not appear on admin interface * Product part number resests to '' on clone

--- django/solonotebooks/cotizador/admin.py
+++ django/solonotebooks/cotizador/admin.py
@@ -118,7 +118,7 @@ admin.site.register(VideoCardMemoryBusWidth)
 admin.site.register(VideoCardGpuCoreCount)
 admin.site.register(VideoCardGpuManufacturingProcess)
 
-excludes = ['ptype', 'shp', 'week_visitor_count', 'week_discount', 'long_description', 'similar_products', 'sponsored_shp', 'week_external_visits', 'display_name']
+excludes = ['ptype', 'shp', 'week_visitor_count', 'week_discount', 'long_description', 'similar_products', 'sponsored_shp', 'week_external_visits', 'display_name', 'part_number']
             
 class ProductAdmin(admin.ModelAdmin):
     exclude = excludes
