43e734c189add74c24a67be499fb7ae52de775f0
Vijay Khemlani <vkhemlan@gmail.com>
2011-09-04 14:39:50 -0300

    Added netbook fetching to Paris script

--- django/solonotebooks/fetch_scripts/paris.py
+++ django/solonotebooks/fetch_scripts/paris.py
@@ -52,6 +52,8 @@ class Paris(FetchStore):
         urls = [
                     # Notebooks
                     ['http://www.paris.cl/webapp/wcs/stores/servlet/categoryTodos_10001_40000000577_-5_51049202_18877035_si_2__18877035,50999203,51049192,51049202_', 'Notebook'],
+                    # Netbooks
+                    ['http://www.paris.cl/webapp/wcs/stores/servlet/categoryTodos_10001_40000000577_-5_51056269_18877035_si_2__18877035,51039699,51056269_', 'Notebook'],
                     # LCD
                     ['http://www.paris.cl/webapp/wcs/stores/servlet/categoryTodos_10001_40000000577_-5_51056205_20096521_si_2__20096521,51056194,51056195,51056205_', 'Screen'],
                     ]
