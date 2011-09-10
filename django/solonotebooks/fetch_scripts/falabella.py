ae19acda45491e9c4dbd92f83fcba601035102be
Vijay Khemlani <vkhemlan@gmail.com>
2011-09-09 23:02:25 -0300

    * Fixed Falabella script

--- django/solonotebooks/fetch_scripts/falabella.py
+++ django/solonotebooks/fetch_scripts/falabella.py
@@ -5,6 +5,7 @@ from BeautifulSoup import BeautifulSoup
 import elementtree.ElementTree as ET
 from elementtree.ElementTree import Element
 from . import ProductData, FetchStore
+import re
 
 class Falabella(FetchStore):
     name = 'Falabella'
@@ -15,42 +16,14 @@ class Falabella(FetchStore):
         product_data = browser.open(product_link).get_data()
         product_soup = BeautifulSoup(product_data)
         
-        avail_div = product_soup.find('div', { 'id' : 'mensajeSinStock' })
-        if avail_div:
+        product_name = product_soup.find('div', { 'id' : 'destacadoRuta' }).find('a').string
+
+        if not product_name:
             return None
-        
-        product_name = product_soup.find('div', { 'id' : 'descripcion-corta' }).find('h1').string.encode('ascii', 'ignore')
-        
-        product_prices = []
-        
-        try:
-            product_price = int(product_soup.find('div', { 'id' : 'precioNormalF' }).string.split('&#36;')[1].replace('.', ''))
-            product_prices.append(product_price)
-        except:
-            pass
-        
-        try:
-            product_price = int(product_soup.find('div', { 'id' : 'precioInternetF' }).string.split('&#36;')[1].replace('.', ''))
-            product_prices.append(product_price)
-        except:
-            pass
-        
-        try:
-            product_price = int(product_soup.find('div', { 'id' : 'precioInternetRF' }).string.split('&#36;')[1].replace('.', ''))
-            product_prices.append(product_price)
-        except:
-            pass
-            
-        try:
-            product_price = int(product_soup.find('div', { 'id' : 'oportunidadF' }).string.split('&#36;')[1].replace('.', ''))
-            product_prices.append(product_price)
-        except:
-            pass
-        
-        if not product_prices:
-            raise Exception
-            
-        product_price = min(product_prices)
+
+        product_name = ' '.join(re.split('\s+', product_name.replace('&nbsp;', ' ').replace('\r', ' ').replace('\n', ' ').replace('\t', ' '))).encode('ascii', 'ignore')
+
+        product_price = int(product_soup.find('div', { 'class' : 'precio1' }).contents[2].replace('.', ''))
             
         product_data = ProductData()
         product_data.custom_name = product_name
@@ -67,34 +40,33 @@ class Falabella(FetchStore):
         # Browser initialization
         browser = mechanize.Browser()
         
-        url_schemas = [    ['http://www.falabella.com/webapp/commerce/command/ExecMacro/falabella/macros/list_prod.d2w/report?cgmenbr=1891&cgrfnbr=2458576&cgpadre=2458457&cghijo=&cgnieto=2458576&division=[page]&orden=&ConFoto=1&pcomp1=&pcomp2=&pcomp3=&pcomp4=&pcomp5=&pcomp6=&pcomp7=&pcomp8=&pcomp9=&pcomp10=&cghijo1=2460493&nivel=1', 'Notebook'],
-                           ['http://www.falabella.com/webapp/commerce/command/ExecMacro/falabella/macros/list_prod.d2w/report?sprod=0&ConFoto=1&cgmenbr=1891&nivel=1&cgrfnbr=2541581&sfot=0&cgpadre=2457964&cgnieto=2541581&cghijo1=2486170&division=[page]', 'Screen'],
+        url_schemas = [    
+                            ['http://www.falabella.com/falabella-cl/browse/productList.jsp?_dyncharset=iso-8859-1&requestChainToken=0005653415&pageSize=16&priceFlag=&categoryId=cat70057&docSort=numprop&docSortProp=price&docSortOrder=ascending&onlineStoreFilter=online&userSelectedFormat=4*4&trail=SRCH%3Acat70057&navAction=jump&searchCategory=true&question=cat70057&qfh_s_s=submit&_D%3Aqfh_s_s=+&qfh_ft=SRCH%3Acat70057&_D%3Aqfh_ft=+&_DARGS=%2Ffalabella-cl%2Fbrowse%2FfacetsFunctions.jsp&goToPage=', 'Notebook'],
+                            ['http://www.falabella.com/falabella-cl/browse/productList.jsp?_dyncharset=iso-8859-1&requestChainToken=0005655243&pageSize=16&priceFlag=&categoryId=cat70043&docSort=numprop&docSortProp=price&docSortOrder=ascending&onlineStoreFilter=online&userSelectedFormat=4*4&trail=SRCH%3Acat70043&navAction=jump&searchCategory=true&question=cat70043&qfh_s_s=submit&_D%3Aqfh_s_s=+&qfh_ft=SRCH%3Acat70043&_D%3Aqfh_ft=+&_DARGS=%2Ffalabella-cl%2Fbrowse%2FfacetsFunctions.jsp&goToPage=', 'Screen'],
+                            ['http://www.falabella.com/falabella-cl/browse/productList.jsp?_dyncharset=iso-8859-1&requestChainToken=0005655389&pageSize=16&priceFlag=&categoryId=cat2053&docSort=numprop&docSortProp=price&docSortOrder=ascending&onlineStoreFilter=online&userSelectedFormat=4*4&trail=SRCH%3Acat2053&navAction=jump&searchCategory=true&question=cat2053&qfh_s_s=submit&_D%3Aqfh_s_s=+&qfh_ft=SRCH%3Acat2053&_D%3Aqfh_ft=+&_DARGS=%2Ffalabella-cl%2Fbrowse%2FfacetsFunctions.jsp&goToPage=', 'Screen'],
+                            ['http://www.falabella.com/falabella-cl/browse/productList.jsp?_dyncharset=iso-8859-1&requestChainToken=0005655389&pageSize=16&priceFlag=&categoryId=cat70044&docSort=numprop&docSortProp=price&docSortOrder=ascending&onlineStoreFilter=online&userSelectedFormat=4*4&trail=SRCH%3Acat70044&navAction=jump&searchCategory=true&question=cat70044&qfh_s_s=submit&_D%3Aqfh_s_s=+&qfh_ft=SRCH%3Acat70044&_D%3Aqfh_ft=+&_DARGS=%2Ffalabella-cl%2Fbrowse%2FfacetsFunctions.jsp&goToPage=', 'Screen'],
                             
                       ]
         product_links = []
                             
         for url_schema, ptype in url_schemas:
-            page_number = 0
+            page_number = 1
             
             while True:
-                url = url_schema.replace('[page]', str(page_number))
+                url = url_schema + str(page_number)
 
                 # Obtain and parse HTML information of the base webpage
                 baseData = browser.open(url).get_data()
                 baseSoup = BeautifulSoup(baseData)
 
                 # Obtain the links to the other pages of the catalog (2, 3, ...)
-                mosaicDivs = baseSoup.findAll("div", { "id" : "mosaico" })
+                mosaicDivs = baseSoup.findAll('div', { 'class':'quickView' })
                 
                 if not mosaicDivs:
                     break
                 
-                for mosaicDiv in mosaicDivs:                        
-                    url = 'http://www.falabella.com' + mosaicDiv.find('a')['href']
-                    base_url, args = url.split('?')
-                    d_args = dict([elem.split('=') for elem in args.split('&')])
-                    del d_args['division']
-                    url = base_url + '?' + '&'.join([key + '=' + value for key, value in d_args.items()])
+                for div in mosaicDivs:                        
+                    url = 'http://www.falabella.com' + div.find('a')['href']
                     
                     product_links.append([url, ptype])
                     
