4972806a3cd57eb14a8aecd417e9a5756cc76072
Vijay Khemlani <vkhemlan@gmail.com>
2011-10-30 13:08:52 -0300

    * Fixed Wei script

--- django/solonotebooks/fetch_scripts/wei.py
+++ django/solonotebooks/fetch_scripts/wei.py
@@ -14,19 +14,18 @@ class Wei(FetchStore):
         data = br.open(product_link).get_data()
         soup = BeautifulSoup(data)
         
-        availabilities = soup.findAll("div", { "class" : "TXTSM" })[1].contents[2::4]
+        availabilities = soup.find('div', { 'class' : 'pdisponibilidad' }).findAll('td')
         
         for availability in availabilities:
-            if 'agotado' in availability:
+            if 'Producto agotado' in availability.contents[1]:
                 return None
-
+        
         productData = ProductData()
 
-        titleSpans = soup.findAll("div", { "class" : "TXTSM" })
-        title = titleSpans[0].string.strip()
+        title = soup.find('title').string.split('WEI CHILE S. A. - ')[1]
 
-        priceCells = soup.findAll('td', { 'class' : 'TXTRBIG' })
-        price = int(priceCells[1].string.strip().replace(',', ''))
+        priceCell = soup.find('th', { 'class' : 'red' })
+        price = int(priceCell.string.strip().replace('.', ''))
         productData.custom_name = title.encode('ascii','ignore')
         productData.price = price
         productData.url = product_link
@@ -38,7 +37,8 @@ class Wei(FetchStore):
     # Main method
     def retrieve_product_links(self):
         # Basic data of the target webpage and the specific catalog
-        urlBase = 'http://www.wei.cl/catalogue/category.htm?action=subcategory&ccode='
+        urlStore = 'http://www.wei.cl/'
+        urlBase = urlStore + 'index.htm?op=categoria&ccode='
         
         # Browser initialization
         browser = mechanize.Browser()
@@ -48,35 +48,36 @@ class Wei(FetchStore):
         links = []
         
         category_urls = [
-                    ['NB&sccode=79', 'Notebook'],      # Notebooks
-                    ['TV&sccode=133', 'VideoCard'],     # Tarjetas de video AGP
-                    ['TV&sccode=134', 'VideoCard'],     # Tarjetas de video PCI Express
-                    ['CP&sccode=57', 'Processor'],     # Procesadores AMD
-                    ['CP&sccode=58', 'Processor'],     # Procesadores Intel
-                    ['MO&sccode=162', 'Screen'],    # LCD TV
-                    ['MO&sccode=19', 'Screen'],     # Monitores LCD
-                    ['MB&sccode=23', 'Motherboard'],     # MB AMD
-                    ['MB&sccode=24', 'Motherboard'],     # MB Intel
+                    ['252', 'Notebook'],      # Notebooks
+                    ['175', 'VideoCard'],     # Tarjetas de video AGP
+                    ['176', 'VideoCard'],     # Tarjetas de video PCI Express
+                    ['119', 'Processor'],     # Procesadores AMD
+                    ['120', 'Processor'],     # Procesadores Intel
+                    ['205', 'Screen'],    # LCD TV
+                    ['80', 'Screen'],     # Monitores LCD
+                    ['65', 'Motherboard'],     # MB AMD
+                    ['84', 'Motherboard'],     # MB Intel
                         ]
-                            
+                     
+        
         for category_url, ptype in category_urls:
-            urlWebpage = urlBase + category_url
-            
-            # Obtain and parse HTML information of the base webpage
-            baseData = browser.open(urlWebpage).get_data()
-            baseSoup = BeautifulSoup(baseData)
-
-            try:
-                productLinks = baseSoup.find('table', { 'cellpadding': '5'}).findAll("a", { "class" : "TXTSMNU" })
-            except AttributeError, e:
-                continue
+            desde = 1
+            while True:
+                urlWebpage = urlBase + category_url + '&desde=' + str(desde)
                 
-            for link in productLinks:
-                link = link['href']
-                if link in links:
-                    continue
-                links.append(link)
-                product_links.append([link, ptype])
+                # Obtain and parse HTML information of the base webpage
+                baseData = browser.open(urlWebpage).get_data()
+                baseSoup = BeautifulSoup(baseData)
+
+                product_cells = baseSoup.findAll('div', { 'class': 'box1'})
+                
+                if not product_cells:
+                    break
+                    
+                for cell in product_cells:
+                    link = urlStore + cell.parent['href']
+                    product_links.append([link, ptype])
+                desde += 20
 
         return product_links
 
