0997874e00a92e2423a19ce21c4b8e117a3cd357
Vijay Khemlani <vkhemlan@gmail.com>
2011-11-26 16:55:10 -0300

    * Fixed HPOnline script

--- django/solonotebooks/fetch_scripts/hp_online.py
+++ django/solonotebooks/fetch_scripts/hp_online.py
@@ -14,8 +14,8 @@ class HPOnline(FetchStore):
         browser = mechanize.Browser()
         product_data = browser.open(product_link).get_data()
         product_soup = BeautifulSoup(product_data)
-        product_name = product_soup.find('span', { 'id': 'ctl00_templateContenido_detalle1_iuNombre' }).string.encode('ascii', 'ignore')
-        product_price = int(product_soup.find('div', { 'id': 'ctl00_templateContenido_detalle1_iuPanelML' }).string.split('$')[1].replace('.', ''))
+        product_name = product_soup.findAll('h2')[-1].string.encode('ascii', 'ignore')
+        product_price = int(product_soup.find('h1', { 'class': 'bigtitle' }).contents[0].split('$')[1].replace('.', ''))
         
         product_data = ProductData()
         product_data.custom_name = product_name
@@ -34,22 +34,19 @@ class HPOnline(FetchStore):
         # Browser initialization
         browser = mechanize.Browser()
         
-        url_extensions = [  ['productos.aspx?cmd=Z2w=', 'Notebook'],
-                            ['productos.aspx?cmd=ZWw=', 'Notebook'],
-                            ['remates.aspx', 'Notebook'],
+        url_extensions = [  ['categoria.aspx?cat=ZA==&V=G', 'Notebook'],
                             ]
                           
         product_links = []                            
         for url_extension, ptype in url_extensions:
             urlWebpage = urlBase + urlBuscarProductos + url_extension
-            
 
             baseData = browser.open(urlWebpage).get_data()
             baseSoup = BeautifulSoup(baseData)
 
-            ntbkCells = baseSoup.findAll('div', { 'class' : 'grid_item' })
+            ntbkCells = baseSoup.findAll('div', { 'class' : 'product' })
             
             for ntbkCell in ntbkCells:
-                product_links.append([urlBase + urlBuscarProductos + ntbkCell.find('a')['href'], ptype])
+                product_links.append([urlBase + ntbkCell.find('a')['href'], ptype])
                 
         return product_links
