d567ed994ed9cf6437c550f73e1313a87a6f4f36
Vijay Khemlani <vkhemlan@gmail.com>
2011-12-03 14:07:31 -0300

    * Changed RAM Capacity from GB to MB

--- django/solonotebooks/cotizador/models/ram_total_capacity.py
+++ django/solonotebooks/cotizador/models/ram_total_capacity.py
@@ -4,7 +4,7 @@ class RamTotalCapacity(models.Model):
     value = models.IntegerField()
     
     def __unicode__(self):
-        return unicode(self.value) + ' GB'
+        return unicode(self.value) + ' MB'
         
     def raw_text(self):
         return unicode(self)      
