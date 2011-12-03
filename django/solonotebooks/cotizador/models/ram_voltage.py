600e44c36ce392c5a9c43558cd044ec3c78bc5a9
Vijay Khemlani <vkhemlan@gmail.com>
2011-12-03 17:34:44 -0300

    * Increased precision on RAM Voltage

--- django/solonotebooks/cotizador/models/ram_voltage.py
+++ django/solonotebooks/cotizador/models/ram_voltage.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class RamVoltage(models.Model):
-    value = models.DecimalField(max_digits=3, decimal_places=1)
+    value = models.DecimalField(max_digits=4, decimal_places=2)
     
     def __unicode__(self):
         return unicode(self.value) + ' V'
