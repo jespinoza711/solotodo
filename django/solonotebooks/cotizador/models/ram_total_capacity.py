8a9a423dbb345673bbbaa4667dfe69c6decbd91f
Vijay Khemlani <vkhemlan@gmail.com>
2011-12-03 16:00:20 -0300

    * Changed the string representation of a RAM product * Added intelligent MB / GB detection to RAM quantities

--- django/solonotebooks/cotizador/models/ram_total_capacity.py
+++ django/solonotebooks/cotizador/models/ram_total_capacity.py
@@ -4,7 +4,10 @@ class RamTotalCapacity(models.Model):
     value = models.IntegerField()
     
     def __unicode__(self):
-        return unicode(self.value) + ' MB'
+        if self.value % 1024 == 0:
+            return unicode(self.value / 1024) + ' GB'
+        else:
+            return unicode(self.value) + ' MB'
         
     def raw_text(self):
         return unicode(self)      
