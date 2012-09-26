54b68c3ddc8b4d82898662a5705783d9d2aabb57
Vijay Khemlani <vkhemlan@gmail.com>
2012-09-26 13:09:54 -0300

    * Fixed Falabella script

--- django/solonotebooks/fetch_scripts/falabella.py
+++ django/solonotebooks/fetch_scripts/falabella.py
@@ -52,21 +52,33 @@ class Falabella(FetchStore):
         
         # Browser initialization
         browser = mechanize.Browser()
-        
-        url_schemas = [    
-                            ['http://www.falabella.com/falabella-cl/browse/productList.jsp?_dyncharset=iso-8859-1&requestChainToken=0005653415&pageSize=16&priceFlag=&categoryId=cat70057&docSort=numprop&docSortProp=price&docSortOrder=ascending&onlineStoreFilter=online&userSelectedFormat=4*4&trail=SRCH%3Acat70057&navAction=jump&searchCategory=true&question=cat70057&qfh_s_s=submit&_D%3Aqfh_s_s=+&qfh_ft=SRCH%3Acat70057&_D%3Aqfh_ft=+&_DARGS=%2Ffalabella-cl%2Fbrowse%2FfacetsFunctions.jsp&goToPage=', 'Notebook'],
-                            ['http://www.falabella.com/falabella-cl/browse/productList.jsp?_dyncharset=iso-8859-1&requestChainToken=0005655243&pageSize=16&priceFlag=&categoryId=cat70043&docSort=numprop&docSortProp=price&docSortOrder=ascending&onlineStoreFilter=online&userSelectedFormat=4*4&trail=SRCH%3Acat70043&navAction=jump&searchCategory=true&question=cat70043&qfh_s_s=submit&_D%3Aqfh_s_s=+&qfh_ft=SRCH%3Acat70043&_D%3Aqfh_ft=+&_DARGS=%2Ffalabella-cl%2Fbrowse%2FfacetsFunctions.jsp&goToPage=', 'Screen'],
-                            ['http://www.falabella.com/falabella-cl/browse/productList.jsp?_dyncharset=iso-8859-1&requestChainToken=0005655389&pageSize=16&priceFlag=&categoryId=cat2053&docSort=numprop&docSortProp=price&docSortOrder=ascending&onlineStoreFilter=online&userSelectedFormat=4*4&trail=SRCH%3Acat2053&navAction=jump&searchCategory=true&question=cat2053&qfh_s_s=submit&_D%3Aqfh_s_s=+&qfh_ft=SRCH%3Acat2053&_D%3Aqfh_ft=+&_DARGS=%2Ffalabella-cl%2Fbrowse%2FfacetsFunctions.jsp&goToPage=', 'Screen'],
-                            ['http://www.falabella.com/falabella-cl/browse/productList.jsp?_dyncharset=iso-8859-1&requestChainToken=0005655389&pageSize=16&priceFlag=&categoryId=cat70044&docSort=numprop&docSortProp=price&docSortOrder=ascending&onlineStoreFilter=online&userSelectedFormat=4*4&trail=SRCH%3Acat70044&navAction=jump&searchCategory=true&question=cat70044&qfh_s_s=submit&_D%3Aqfh_s_s=+&qfh_ft=SRCH%3Acat70044&_D%3Aqfh_ft=+&_DARGS=%2Ffalabella-cl%2Fbrowse%2FfacetsFunctions.jsp&goToPage=', 'Screen'],
-                            
-                      ]
+
+        base_url = ''\
+                   'http://www.falabella.com/falabella-cl/browse/productList.jsp?'\
+                   '_dyncharset=iso-8859-1&requestChainToken=0422806850&pageSize=16&'\
+                   'priceFlag=&categoryId=cat{0}&docSort=numprop&docSortProp=price'\
+                   '&docSortOrder=ascending&onlineStoreFilter=online&'\
+                   'userSelectedFormat=4*4&trail=SRCH%3Acat{0}&navAction=jump&'\
+                   'searchCategory=true&question=cat{0}&qfh_s_s=submit&_D%3Aqfh_s_s'\
+                   '=+&qfh_ft=SRCH%3Acat{0}&_D%3Aqfh_ft=+&_DARGS=%2Ffalabella-cl%2F'\
+                   'browse%2FfacetsFunctions.jsp.searchFacetsForm&goToPage='
+
+        url_schemas = [
+            ['70057', 'Notebook'],
+            ['70054', 'Notebook'],
+            ['1000006', 'Notebook'],
+            ['2053', 'Television'],
+            ['70043', 'Television'],
+            ['70044', 'Television'],
+            ]
+
         product_links = []
                             
         for url_schema, ptype in url_schemas:
             page_number = 1
             
             while True:
-                url = url_schema + str(page_number)
+                url = base_url.format(url_schema) + str(page_number)
 
                 # Obtain and parse HTML information of the base webpage
                 baseData = browser.open(url).get_data()
